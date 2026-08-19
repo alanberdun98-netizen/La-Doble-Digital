470d2b1f955028e1d4638c9da6445a3002c73475
Alan Berdun <alanberdun@gmail.com>
2026-08-19 11:01:41 -0300

    actualizacion

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("../"));
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("La Doble Digital - Backend funcionando 🚀");
+app.get("/api/noticias", (req, res) => {
+    res.json([
+        {
+            titulo: "primera noticia",
+            categoria: "politica"
+        }
+    ]);
 });
+
+app.get("/", (req, res) => {
+    const path = require("path");
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en http://localhost:${PORT}`);
